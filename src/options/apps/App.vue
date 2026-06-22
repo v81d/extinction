7af80b2129cc1fa64934d5ad20be35cc8982b3c2
v81d <v81d@proton.me>
2026-06-22 19:25:27 -0400

    fix(options): fix background

--- src/options/apps/App.vue
+++ src/options/apps/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
+  <div
+    class="flex flex-col items-center w-full h-full text-base bg-(--color-bg-primary)"
+  >
     <!-- options go here ... -->
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+* {
+  --color-bg-primary: #212b4f;
+  --color-text-primary: #aab9ed;
+}
+
+p,
+span,
+button {
+  color: var(--color-text-primary);
+}
+</style>
