4b7ab67fae80770f0d043784bcd18374229659ad
v81d <v81d@proton.me>
2026-06-23 19:16:37 -0400

    feat(options): add header and main view

---
+++ components/options/TabBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex gap-1 p-1 rounded-lg bg-zinc-700/50">
+    <button
+      v-for="(tab, i) in props.tabs"
+      :key="i"
+      class="cursor-pointer px-4 py-2 text-sm text-zinc-200 rounded-lg"
+      :class="
+        i === props.state.activeTab.value
+          ? 'bg-zinc-600'
+          : 'hover:bg-zinc-600/50'
+      "
+      @click="props.state.activeTab.value = i"
+    >
+      {{ tab }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  tabs: string[];
+  state: {
+    activeTab: Ref<number>;
+  };
+}>();
+</script>
+
+<style scoped></style>
--- entrypoints/options/App.vue
+++ entrypoints/options/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <div
-    class="flex flex-col gap-4 p-8 w-full h-full text-base bg-(--color-bg-primary)"
-  >
-    <h1 class="text-4xl font-bold">Extinction Options</h1>
-    <p>
-      There are no configurable options yet. This feature is still
-      work-in-progress.
-    </p>
+  <div class="flex flex-col w-full h-full text-zinc-200 bg-zinc-900">
+    <header class="flex items-center gap-4 px-4 py-2 w-full bg-zinc-800">
+      <img src="/icon/32.png" alt="Extinction" title="Extinction" />
+      <TabBar :tabs="TABS" :state="tabBarState" />
+    </header>
+
+    <main class="flex-1"></main>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { directive as VTippy } from "vue-tippy";
+import "tippy.js/dist/tippy.css";
+
+import TabBar from "@/components/options/TabBar.vue";
+
+/** The array of tabs to show in the header. */
+const TABS = ["About", "Trusted Sites"];
+
+/** The tab bar state. */
+const tabBarState = {
+  activeTab: ref<number>(0),
+};
+</script>
 
 <style scoped></style>
