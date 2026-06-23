b175170e2755ffef59a44502b15ff4d29445e86d
v81d <v81d@proton.me>
2026-06-23 10:21:55 -0400

    feat(options): add WIP notice to options page

--- src/options/apps/App.vue
+++ src/options/apps/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div
-    class="flex flex-col items-center w-full h-full text-base bg-(--color-bg-primary)"
+    class="flex flex-col gap-4 p-8 w-full h-full text-base bg-(--color-bg-primary)"
   >
-    <!-- options go here ... -->
+    <h1 class="text-4xl font-bold">Extinction Options</h1>
+    <p>
+      There are no configurable options yet. This feature is still
+      work-in-progress.
+    </p>
   </div>
 </template>
 
@@ -14,6 +18,13 @@
   --color-text-primary: #aab9ed;
 }
 
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+a,
 p,
 span,
 button {
